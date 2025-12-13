479e1e18bcc7a9eeee49e036e6c487cb1d3fe4cf
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2025-12-13 19:11:39 +0100

    Retry Policy for SearchService to re-consume the message from the queue - 5 retries with delay of 5 second between each. If still dont consume it , the message will end up in rabbit's error queue

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -24,9 +24,14 @@ builder.Services.AddMassTransit(x =>
 {
     // Where to find consumers that we are creating:
     x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
-    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));    
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
         cfg.ConfigureEndpoints(context);
     });
 });
