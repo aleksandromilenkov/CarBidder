17418812611cf392c3288f0ce41589967a6b0ef1
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2025-12-16 00:37:21 +0100

    setting up the claims that need to be included in the access token - username claim and name claim

---
+++ src/IdentityService/Services/CustomProfileService.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+using Duende.IdentityModel;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService.Services
+{
+    public class CustomProfileService(UserManager<ApplicationUser> _userManager) : IProfileService
+    {
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+           var user = await _userManager.GetUserAsync(context.Subject);
+           var existingClaims = await _userManager.GetClaimsAsync(user);
+
+            var claims = new List<Claim>
+            {
+                new Claim("username", user.UserName),
+            };
+
+            context.IssuedClaims.AddRange(claims);
+            context.IssuedClaims.Add(existingClaims.FirstOrDefault(c => c.Type == JwtClaimTypes.Name));
+        }
+
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
