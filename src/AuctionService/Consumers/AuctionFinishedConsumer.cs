3e9a2efb3f604243172cedbf3f8e3f184314b3dd
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2025-12-17 00:15:45 +0100

    Consuming Events for AuctionFinished and BidPlaced in AuctionSerivce and SearchService and Configuring a NextJs Client in the IdentityService for our Frontend App

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,23 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionFinishedConsumer(AuctionDbContext _dbContext) : IConsumer<AuctionFinished>
+    {
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+        {
+            Console.WriteLine("--> Consuming auction finished");
+            var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+            if(context.Message.ItemSold)
+            {
+                auction.Winner = context.Message.Winner;
+                auction.SoldAmount = context.Message.Amount;
+            }
+
+            auction.Status = auction.SoldAmount > auction.ReservePrice ? Enums.Status.Finished : Enums.Status.ReserveNotMet;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
