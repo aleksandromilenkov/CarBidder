ab0f2bbb869bfff1de80a111611e809eba90db5d
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2025-12-28 18:10:22 +0100

    Bidding Service - dotnet web api, setup, cleanup and adding the entitties

---
+++ src/BiddingService/Program.cs
@@ -0,0 +1,49 @@
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("bids", false));
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h => {
+            h.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest")); // if not specified RabbitMq:Username in the config, use the word 'guest'
+            h.Username(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+// Resolve authority from either Docker config key or local appsettings
+var authority = builder.Configuration["IdentityServiceUrl"] ?? builder.Configuration["IdentityServer:Url"];
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = authority;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await DB.InitAsync("BidDb",
+    MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("BidDbConnection")));
+
+app.Run();
