c8d2811580045750e24b498704e21d509381a86c
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2025-12-28 18:54:25 +0100

    Bids Controller - Placing Bid and Getting Bids for Auction

---
+++ src/BiddingService/Controllers/BidsController.cs
@@ -0,0 +1,70 @@
+﻿using BiddingService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+
+namespace BiddingService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BidsController : ControllerBase
+    {
+        [HttpGet("{auctionId}")]
+        public async Task<ActionResult<List<Bid>>> GetBidsForAuction([FromRoute] string auctionId)
+        {
+            var bids = await DB.Find<Bid>()
+                .Match(b => b.AuctionId == auctionId)
+                .Sort(b => b.Descending(b => b.BidTime))
+                .ExecuteAsync();
+            return Ok(bids);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Bid>> PlaceBid([FromQuery] string auctionId, [FromQuery] int amount)
+        {
+            var auction = await DB.Find<Auction>().OneAsync(auctionId);
+
+            if(auction == null)
+            {
+                // TODO: Check with AuctionService if that has auction
+                return NotFound("Auction not found");
+            }
+
+            if (auction.Seller == User.Identity.Name)
+            {
+                return BadRequest("Seller cannot place bids on their own auction");
+            }
+
+            var bid = new Bid
+            {
+                AuctionId = auctionId,
+                Bidder = User.Identity.Name ?? "anonymous",
+                Amount = amount
+            };
+
+            if(auction.AuctionEnd < DateTime.UtcNow)
+            {
+                bid.BidStatus = BidStatus.Finished;
+            } else {
+                var highBid = await DB.Find<Bid>()
+                   .Match(b => b.AuctionId == auctionId)
+                   .Sort(b => b.Descending(b => b.Amount))
+                   .ExecuteFirstAsync();
+
+                if ((highBid != null && amount > highBid.Amount) || highBid == null)
+                {
+                    bid.BidStatus = amount > auction.ReservePrice ? BidStatus.Accepted : BidStatus.AcceptedBelowReserve;
+                }
+
+                if (highBid != null && amount <= highBid.Amount)
+                {
+                    bid.BidStatus = BidStatus.TooLow;
+                }
+            }
+            await DB.SaveAsync(bid);
+            return Ok(bid);
+        } 
+    }
+}
--- src/BiddingService/Models/Bid.cs
+++ src/BiddingService/Models/Bid.cs
@@ -5,7 +5,7 @@ namespace BiddingService.Models
     public class Bid : Entity
     {   
         public string AuctionId { get; set; }
-        public string Bider { get; set; }
+        public string Bidder { get; set; }
         public DateTime BidTime { get; set; } = DateTime.UtcNow;
         public int Amount { get; set; }
         public BidStatus BidStatus { get; set; }
