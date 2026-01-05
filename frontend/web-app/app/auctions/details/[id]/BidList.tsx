5f548adb368512d81a7e2af3e14239362e1e3118
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2026-01-05 18:45:44 +0100

    conditionally display bid form

--- frontend/web-app/app/auctions/details/[id]/BidList.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidList.tsx
@@ -19,7 +19,6 @@ const BidList = ({ user, auction }: Props) => {
   const [loading, setLoading] = useState(true);
   const bids = useBidStore((state) => state.bids);
   const setBids = useBidStore((state) => state.setBids);
-
   const highestBid = bids.reduce(
     (max, bid) => (bid.amount > max ? bid.amount : max),
     0
@@ -70,7 +69,17 @@ const BidList = ({ user, auction }: Props) => {
         )}
       </div>
       <div className="px-2 pb-2 text-gray-500">
-        <BidForm auctionId = {auction.id} highestBid={highestBid}/>
+        {!user ? (
+          <div className="flex items-center justify-center p-2 text-lg font-semibold">
+            Please login to make a bid.
+          </div>
+        ) : user && user.username === auction.seller ? (
+          <div className="flex items-center justify-center p-2 text-lg font-semibold">
+            You cannot bid on your own auction.
+          </div>
+        ) : (
+          <BidForm auctionId={auction.id} highestBid={highestBid} />
+        )}
       </div>
     </div>
   );
