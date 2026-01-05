6ab358ce57111a9cf19bc8a4ba778f23d7a58f6c
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2026-01-05 21:00:55 +0100

    asynchronously displaying toast for AuctionFinished

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -1,13 +1,15 @@
-'use client';
+"use client";
 import { useAuctionStore } from "@/hooks/useAuctionStore";
 import { useBidStore } from "@/hooks/useBidStore";
-import { Auction, Bid } from "@/types";
+import { Auction, AuctionFinished, Bid } from "@/types";
 import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
 import { User } from "next-auth";
 import { useParams } from "next/navigation";
 import { useCallback, useEffect, useRef } from "react";
 import AuctionCreatedToast from "../components/AuctionCreatedToast";
 import toast from "react-hot-toast";
+import AuctionFinishedToast from "../components/AuctionFinishedToast";
+import { getDetailedViewData } from "../actions/auctionActions";
 
 type Props = {
   children: React.ReactNode;
@@ -19,20 +21,51 @@ const SignalRProvider = ({ children, user }: Props) => {
   const addBid = useBidStore((state) => state.addBid);
   const params = useParams<{ id: string }>();
 
-  const handleBidPlaced = useCallback((bid: Bid) => {
-     if(bid.bidStatus.includes("Accepted")){
+  const handleBidPlaced = useCallback(
+    (bid: Bid) => {
+      if (bid.bidStatus.includes("Accepted")) {
         setCurrentPrice(bid.auctionId, bid.amount);
-     }
-     if(params.id === bid.auctionId){
+      }
+      if (params.id === bid.auctionId) {
         addBid(bid);
-     }
-  }, [addBid, setCurrentPrice, params.id]);
+      }
+    },
+    [addBid, setCurrentPrice, params.id]
+  );
 
-  const handleAuctionCreated = useCallback((auction: Auction) => {
-    if (user?.username !== auction.seller) {
-        return toast(<AuctionCreatedToast auction={auction} />, { duration: 10000 } );
-    }
-  }, [user?.username]);
+  const handleAuctionCreated = useCallback(
+    (auction: Auction) => {
+      if (user?.username !== auction.seller) {
+        return toast(<AuctionCreatedToast auction={auction} />, {
+          duration: 10000,
+        });
+      }
+    },
+    [user?.username]
+  );
+
+  const handleAuctionFinished = useCallback(
+    (auctionFinished: AuctionFinished) => {
+      const auction = getDetailedViewData(auctionFinished.auctionId);
+      if (user?.username !== auctionFinished.seller) {
+        return toast.promise(
+          auction,
+          {
+            loading: "Loading",
+            success: (auction) => (
+              <AuctionFinishedToast
+                auction={auction}
+                finishedAuction={auctionFinished}
+              />
+            ),
+            error: "Auction finished",
+          },
+          { duration: 10000, icon: null }
+        );
+      }
+    },
+    [user?.username]
+  );
 
   useEffect(() => {
     if (!connection.current) {
@@ -40,21 +73,21 @@ const SignalRProvider = ({ children, user }: Props) => {
         .withUrl("http://localhost:6001/notifications")
         .withAutomaticReconnect()
         .build();
-      connection.current.start()
+      connection.current
+        .start()
         .then(() => console.log("SignalR Connected"))
         .catch((error) => console.error("SignalR Connection Error: ", error));
     }
     connection.current.on("BidPlaced", handleBidPlaced);
     connection.current.on("AuctionCreated", handleAuctionCreated);
+    connection.current.on("AuctionFinished", handleAuctionFinished);
 
     return () => {
-        connection.current?.off("BidPlaced", handleBidPlaced);
-        connection.current?.off("AuctionCreated", handleAuctionCreated);
-    }
-  }, [handleBidPlaced, handleAuctionCreated]);
-  return <div>
-
-    {children}
-    </div>;
+      connection.current?.off("BidPlaced", handleBidPlaced);
+      connection.current?.off("AuctionCreated", handleAuctionCreated);
+      connection.current?.off("AuctionFinished", handleAuctionFinished);
+    };
+  }, [handleBidPlaced, handleAuctionCreated, handleAuctionFinished]);
+  return <div>{children}</div>;
 };
 export default SignalRProvider;
