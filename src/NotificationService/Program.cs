5f4bec2c6cc5b65fa1f67c834e1f9b11ca4521a3
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2026-01-31 17:47:26 +0100

    deploying the notify service to k8s

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -10,9 +10,10 @@ builder.Services.AddMassTransit(x =>
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
     x.UsingRabbitMq((context, cfg) =>
     {
-        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h => {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h =>
+        {
             h.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest")); // if not specified RabbitMq:Username in the config, use the word 'guest'
-            h.Username(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+            h.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
         });
         cfg.ConfigureEndpoints(context);
     });
